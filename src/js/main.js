a7623b2be5d9b2ce431360dc38663d8d61949c8b
Heba Farestveit Úlfarsdóttir <heba.farestveit@gmail.com>
2021-11-12 11:33:21 +0000

    Update sass and added comments.

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,8 @@
-window.onload = function() {
-    document.body.className += " loaded";
-}
+const mouseFollow = document.getElementById('mouseFollow');
+
+document.addEventListener('mousemove', (e) => {
+  mouseFollow.style.cssText = `
+    left: ${e.clientX + 20}px;
+    top:  ${e.clientY + 20}px;
+  `;
+});
