07345e90c8d19dabd41a3036c1af7e307017476b
Heba Farestveit Úlfarsdóttir <heba.farestveit@gmail.com>
2021-11-12 16:25:22 +0000

    Update comments for code

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,14 @@
+// JS function to make element follow mouse movement
+
+// Get element from DOM by id
 const mouseFollow = document.getElementById('mouseFollow');
 
+// Add event listener on mouse move
 document.addEventListener('mousemove', (e) => {
+
+  // Get the position of the mouse in the viewport and add those positions to 
+  // the mouseFollow element.
+  // This can be seen when you inspect the Html element in the browser.
   mouseFollow.style.cssText = `
     left: ${e.clientX + 20}px;
     top:  ${e.clientY + 20}px;
